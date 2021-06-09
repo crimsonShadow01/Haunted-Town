a23bf43531f8d7863ba074a5946ecc10234f424b
mmorgan00 <70239367+mmorgan00@users.noreply.github.com>
2021-06-09 14:51:34 -0600

    Began work on a gamemanager object

---
+++ Haunted Town/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Game details
+    private playerList players;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
