1adc3d35dd5e1fcb9c8d31626ffc3401b0099e26
mmorgan00 <70239367+mmorgan00@users.noreply.github.com>
2021-06-10 15:19:54 -0600

    Blocked out some pseudo code for handling combat. Thinking a queue will work to handle turn order, given it's going to be fixed after game starts, and then can handle ordering itself on it's own. Clicking buttons/actions will be handled via events, just need to figure out how to 'access' what button is being clicked.

---
+++ Haunted Town/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the button it's assigned to
+        //Get the current character to know what the skill will actually do.
+        //change UI maybe
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+
+    //Event OnButtonClick
+
+    //define event here, game manager will listen to event 
+}
--- Haunted Town/Assets/Scripts/GameManager.cs
+++ Haunted Town/Assets/Scripts/GameManager.cs
@@ -5,16 +5,45 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     //Game details
+    //need to actually define this and flesh it out more. 
     private playerList players;
+
+    //listen to event here for all the things (attack click, spell click, etc)
 
     void Start()
     {
-        
+        //assign players to the list based on speed/agility. 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       //probably won't use this, given we're turn based. but we'll see
+    }
+
+
+}
+
+
+internal class playerList
+{
+    //Some data structure to order them in. Needs to support indexing, but also be flexible. Queue might work fine but look into it
+
+    //FIx this: shouldn't return void. needs to let gamemanager know who is next
+    public void getFirst()
+    {
+
+    }
+
+    //Should get first, shuffle them to last, update order. que/deque or something like that if we use a queue
+    public void takeTurn()
+    {
+
+    }
+
+    //Take some player, and move them to a spot in the turn order. Might not be needed but gives us the opportunity to implement if we want.
+    public void moveTo(int turnSpot)
+    {
+
     }
 }
