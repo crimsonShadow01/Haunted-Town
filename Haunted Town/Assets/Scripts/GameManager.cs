2ff8455591479a30ae1fb14c44dc2e3a6d32634b
mmorgan00 <70239367+mmorgan00@users.noreply.github.com>
2021-06-15 17:53:25 -0600

    BUTTONS WORK....as in buttons have a registered method to them that says 'you clicked this button' in the console

--- Haunted Town/Assets/Scripts/GameManager.cs
+++ Haunted Town/Assets/Scripts/GameManager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //Game details
-    public GameObject playerSpawn;
+    //**Gameobjects we will need to reference**//
+
+    //'player' prefabs
+    public GameObject enemyPlayer;
+    public GameObject allyPlayer;
+    //Panels
+    public GameObject combatMenu;
+    //Game States
+    public enum State { COMBAT, GAMEOVER, VICTORY, LOADLEVEL, MENU, ALLYTURN, ENEMYTURN}
+    State gameState;
 
     //need to actually define this and flesh it out more. 
     private playerList players;
@@ -14,6 +22,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        SwitchState(State.ALLYTURN);
         //assign players to the list based on speed/agility. 
     }
 
@@ -24,6 +33,79 @@ public class GameManager : MonoBehaviour
     }
 
 
+    //**STATE SWITCHING**//
+
+
+    public void SwitchState(State newState)
+    {
+        EndState();
+        BeginState(newState);
+
+    }
+
+    void BeginState(State newState)
+    {
+        switch (newState)
+        {
+            case State.MENU:
+                break;
+            case State.VICTORY:
+                break;
+            case State.GAMEOVER:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.COMBAT:
+                break;
+            case State.ALLYTURN:
+                //Activate the combat menu, 'select' the current turn
+                combatMenu.SetActive(true);
+                break;
+            case State.ENEMYTURN:
+                break;
+        }
+    }
+
+    void EndState()
+    {
+        switch (gameState)
+        {
+            case State.MENU:
+                //If we want a start button menu, do that here
+                break;
+            case State.VICTORY:
+                //Display Victory message
+                break;
+            case State.GAMEOVER:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.COMBAT:
+                break;
+        }
+    }
+
+    //**BUTTON CLICK HANDLERS**//
+
+    public void AttackClicked()
+    {
+        Debug.Log("Attack button clicked");
+    }
+
+    public void MagicClicked()
+    {
+        Debug.Log("Magic button clicked");
+    }
+
+    public void DefendClicked()
+    {
+        Debug.Log("Defed button clicked");
+    }
+
+    public void ItemClicked()
+    {
+        Debug.Log("Item button clicked");
+    }
 }
 
 
